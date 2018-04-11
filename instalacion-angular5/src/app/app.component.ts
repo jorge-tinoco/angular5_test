e0191a94e42d5d78dab3afc8937ec8cfddde1050
JORGE TINOCO <jorgeluis9211@hotmail.com>
2018-04-11 09:57:57 -0500

    Primer commit en git para angular5

---
+++ instalacion-angular5/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tino first app';
+  public Description = 'Descripción breve del compoennte';
+}
